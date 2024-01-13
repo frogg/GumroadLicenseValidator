3be7ce1ce4f5a56472ad70bdd9e0a034b4b0fad7
Frederik Riedel <frederik.riedel@frogg.io>
2024-01-13 16:24:03 +0100

    Update GumroadClient.swift

--- Sources/GumroadLicenseValidator/GumroadClient.swift
+++ Sources/GumroadLicenseValidator/GumroadClient.swift
@@ -14,13 +14,15 @@ import Foundation
 /// - note: Has a configurable property `disputedPurchaseInvalidatesLicense`
 public final class GumroadClient {
     let productPermalink: String
+    let productId: String
 
     /// Initializes only if product permalink string is not empty
     ///
     /// If your product URL is "https://gumroad.com/l/QMGY" your product permalink would be "QMGY."
-    public init?(productPermalink: String) {
+    public init?(productPermalink: String, productId: String) {
         guard productPermalink.isEmpty == false else { return nil }
         self.productPermalink = productPermalink
+        self.productId = productId
     }
 
     /// Checks validity of Gumroad-issued license key
@@ -48,7 +50,7 @@ public final class GumroadClient {
     ///   - licenseKey: Non-empty string, preferably sanitized (remember Little Bobby Tables!)
     ///   - incrementUsesCount: Whether Gumroad should increment count of times a license has been checked
     /// - Returns: `URLRequest` with needed POST parameters
-    func makeRequest(licenseKey: String, incrementUsesCount: Bool = true) -> URLRequest? {
+    func makeRequest(licenseKey: String, productId: String, incrementUsesCount: Bool = true) -> URLRequest? {
         guard productPermalink.isEmpty == false, licenseKey.isEmpty == false else { return nil }
         guard let baseURL = URL(string: "https://api.gumroad.com/v2/licenses/verify"),
               var components = URLComponents(url: baseURL, resolvingAgainstBaseURL: false)
@@ -58,7 +60,8 @@ public final class GumroadClient {
         // https://stackoverflow.com/a/58356848
         components.queryItems = [
             URLQueryItem(name: "product_permalink", value: productPermalink),
-            URLQueryItem(name: "license_key", value: licenseKey)
+            URLQueryItem(name: "license_key", value: licenseKey),
+            URLQueryItem(name: "product_id", value: productId)
         ]
         if incrementUsesCount == false {
             components.queryItems?.append(URLQueryItem(name: "increment_uses_count", value: "false"))
